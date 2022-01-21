c22bf389a5e672878f417a16cefc7373fe24d005
aidancer55 <bluebonnets77@gmail.com>
2022-01-21 20:48:21 +0300

    Добавьте файлы проекта.

---
+++ ArrayNxN/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayNxN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[,] array = new int[N, N];
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write(1);
+                    }
+                    else
+                    {
+                        Console.Write(0);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ex5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int sum=0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                sum += array[i];
+            }
+            int S = sum / 7;
+            Console.WriteLine("Среднее арифметическое чисел равно {0}", S);
+            Console.ReadKey();
+        }
+    }
+}
